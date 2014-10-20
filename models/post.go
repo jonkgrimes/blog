cb2ca5e138510d4694fbf5e55cdf5f9518f4fe68
Jon Grimes <jonkgrimes@gmail.com>
2014-10-20 07:22:06 -0500

    I think I'm getting the hang of this

--- models/post.go
+++ models/post.go
@@ -5,7 +5,7 @@ import "time"
 type Post struct {
 	Id        int64
 	Title     string `sql:"size:255"`
-	Body      string `sql:"text"`
+	Body      string
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
--- server.go
+++ server.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"log"
+
 	"github.com/go-martini/martini"
 	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
 	"github.com/martini-contrib/render"
 )
+
+type Post struct {
+	Id    int64
+	Title string `sql:"size:255"`
+	Body  string `sql:"text"`
+}
+
+type HomePageView struct {
+	Posts []Post
+}
 
 func main() {
 	m := martini.Classic()
@@ -12,15 +25,30 @@ func main() {
 		Layout: "layout",
 	}))
 
+	db := initDb()
+
 	m.Get("/", func(r render.Render) {
-		r.HTML(200, "home", nil)
+		var posts []Post
+		db.Find(&posts)
+		r.HTML(200, "home", &HomePageView{Posts: posts})
 	})
 
 	m.Run()
 }
 
-func initDb() *gorm.DB {
-	db, err := gorm.Open("postgres", "user=root dbname=blog_development sslmode=disable")
+func initDb() gorm.DB {
+	db, err := gorm.Open("postgres", "user=jonkgrimes dbname=blog_development sslmode=disable")
+
+	checkErr(err, "gorm.Open failed")
+
+	db.AutoMigrate(&Post{})
+	db.LogMode(true)
 
 	return db
+}
+
+func checkErr(err error, msg string) {
+	if err != nil {
+		log.Fatalln(msg, err)
+	}
 }
