5bde8790e93e8cedee82db02884372c22a7028f8
Jon Grimes <jonkgrimes@gmail.com>
2015-01-02 18:44:06 -0600

    Playing with defining some helper methods

--- models.go
+++ models.go
@@ -18,3 +18,8 @@ type PostForm struct {
 type HomePageView struct {
 	Posts []Post
 }
+
+func (p Post) PrettyCreatedAt() string {
+	const layout = "Jan 2, 2006 at 3:04pm"
+	return p.CreatedAt.Format(layout)
+}
