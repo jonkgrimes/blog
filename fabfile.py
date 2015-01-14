40a593da60da6ec956046c5027eb619c193a4e35
Jon Grimes <jonkgrimes@gmail.com>
2015-01-13 18:29:53 -0600

    Deploy script working better

--- fabfile.py
+++ fabfile.py
@@ -3,11 +3,15 @@ from fabric.api import *
 from fabric.contrib.console import confirm
 
 env.hosts = ["deploy@jonkgrimes.com"]
+env.forward_agent = True
+
+code_dir = '/var/www/blog'
 
 def deploy():
-    code_dir = '/var/www/blog'
     with settings(warn_only=True):
         if run("test -d %s" % code_dir).failed:
+            sudo("mkdir %s" % code_dir)
+            sudo("chown deploy %s" % code_dir)
             run("git clone git@github.com:jonkgrimes/blog.git %s" % code_dir)
     with cd(code_dir):
         run("git pull")
