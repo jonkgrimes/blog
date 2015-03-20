7b8610a3d1aaf8fee37528cc981b19cde9ded4fe
Jon Grimes <jong@spiceworks.com>
2015-03-20 15:44:15 -0500

    Get tests working

--- models/models_test.go
+++ models/models_test.go
@@ -1,42 +1,43 @@
 package models
 
 import (
+	"blog/models"
 	"testing"
 	"time"
 )
 
 var createSlugCases = []struct {
-	post     Post
+	post     models.Post
 	expected string
 }{
 	{
-		Post{Title: "A Title For A Post"},
+		models.Post{Title: "A Title For A Post"},
 		"a-title-for-a-post",
 	},
 	{
-		Post{Title: "Some punctuation's in this one"},
+		models.Post{Title: "Some punctuation's in this one"},
 		"some-punctuation-s-in-this-one",
 	},
 	{
-		Post{Title: "  This will have   uneven   spaces   "},
+		models.Post{Title: "  This will have   uneven   spaces   "},
 		"this-will-have-uneven-spaces",
 	},
 	{
-		Post{Title: "This Replaces & with and"},
+		models.Post{Title: "This Replaces & with and"},
 		"this-replaces-and-with-and",
 	},
 }
 
 var publishedAtCases = []struct {
-	post     Post
+	post     models.Post
 	expected string
 }{
 	{
-		Post{Title: "A Title of a Post", PublishedAt: time.Date(2015, time.May, 3, 8, 0, 0, 0, time.UTC)},
+		models.Post{Title: "A Title of a Post", PublishedAt: time.Date(2015, time.May, 3, 8, 0, 0, 0, time.UTC)},
 		"May 3, 2015 at 8:00am",
 	},
 	{
-		Post{Title: "A Title of a Post", PublishedAt: time.Date(2015, time.May, 3, 8, 0, 0, 0, time.UTC)},
+		models.Post{Title: "A Title of a Post", PublishedAt: time.Date(2015, time.May, 3, 8, 0, 0, 0, time.UTC)},
 		"May 3, 2015 at 8:00am",
 	},
 }
@@ -57,7 +58,7 @@ func TestCreateSlug(t *testing.T) {
 func TestPrettyPublishedAt(t *testing.T) {
 	t.Log("TestPublishedAt")
 	for _, testCase := range publishedAtCases {
-		post_view := PostView{Post: testCase.post}
+		post_view := models.PostView{Post: testCase.post}
 		actual := post_view.PrettyPublishedAt()
 		expected := testCase.expected
 		if expected != actual {
