37e6fa1fe4cb0e188bd2eb88eaa49586f3c0d53a
Jon Grimes <jong@spiceworks.com>
2015-05-01 09:51:40 -0500

    Split out posts test

--- models/models_test.go
+++ models/models_test.go
@@ -5,28 +5,6 @@ import (
 	"time"
 )
 
-var createSlugCases = []struct {
-	post     Post
-	expected string
-}{
-	{
-		Post{Title: "A Title For A Post"},
-		"a-title-for-a-post",
-	},
-	{
-		Post{Title: "Some punctuation's in this one"},
-		"some-punctuations-in-this-one",
-	},
-	{
-		Post{Title: "  This will have   uneven   spaces   "},
-		"this-will-have-uneven-spaces",
-	},
-	{
-		Post{Title: "This Replaces & with and"},
-		"this-replaces-and-with-and",
-	},
-}
-
 var publishedAtCases = []struct {
 	post     Post
 	expected string
@@ -39,20 +17,6 @@ var publishedAtCases = []struct {
 		Post{Title: "A Title of a Post", PublishedAt: time.Date(2015, time.May, 3, 8, 0, 0, 0, time.UTC)},
 		"May 3, 2015 at 8:00am",
 	},
-}
-
-func TestCreateSlug(t *testing.T) {
-	t.Log("TestCreateSlug")
-	for _, testCase := range createSlugCases {
-		post := testCase.post
-		post.CreateSlug()
-		actual := post.Slug
-		expected := testCase.expected
-		if expected != actual {
-			t.Logf("Expected \"%s\" but got \"%s\"", expected, actual)
-			t.Fail()
-		}
-	}
 }
 
 func TestPrettyPublishedAt(t *testing.T) {
---
+++ models/post_test.go
@@ -0,0 +1,41 @@
+package models
+
+import (
+	"testing"
+)
+
+var createSlugCases = []struct {
+	post     Post
+	expected string
+}{
+	{
+		Post{Title: "A Title For A Post"},
+		"a-title-for-a-post",
+	},
+	{
+		Post{Title: "Some punctuation's in this one"},
+		"some-punctuations-in-this-one",
+	},
+	{
+		Post{Title: "  This will have   uneven   spaces   "},
+		"this-will-have-uneven-spaces",
+	},
+	{
+		Post{Title: "This Replaces & with and"},
+		"this-replaces-and-with-and",
+	},
+}
+
+func TestCreateSlug(t *testing.T) {
+	t.Log("TestCreateSlug")
+	for _, testCase := range createSlugCases {
+		post := testCase.post
+		post.CreateSlug()
+		actual := post.Slug
+		expected := testCase.expected
+		if expected != actual {
+			t.Logf("Expected \"%s\" but got \"%s\"", expected, actual)
+			t.Fail()
+		}
+	}
+}
